2fa95190cb3177b2d2d9b8c45834adac869f24aa
Ceoxlex175 <familink.elijah@gmail.com>
2026-07-07 12:01:43 +0800

    Create ashleyai_user.py

---
+++ ashleyai_user.py
@@ -0,0 +1,7 @@
+user = [ "name", "middlename", "lastname", "age", "address", "location", "phonenumber" ]
+mail = ""
+
+if ( user.middlename === "" ):
+  user = [ "name", "lastname", "age", "address", "location", "phonenumber" ]
+  user = [ user.name = "", user.lastname = "", user.age = "", user.address = "", user.locaion ="", user.phonenumber = "", ]
+  
